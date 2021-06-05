f7d357a6f967f5ae22fa8bc2b5553652f506ec27
Chris Deuda <christopherdeuda@gmail.com>
2021-06-06 02:31:03 +0800

    Hide timestime of the announcement

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Announcement extends Model
 {
     protected $fillable = ['title','content', 'startDate', 'endDate'];
+
+    protected $hidden = ['created_at', 'updated_at'];
 }
