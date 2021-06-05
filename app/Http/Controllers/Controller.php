da96e69a03be7237e6e52dd693bae210412127ad
Chris Deuda <christopherdeuda@gmail.com>
2021-06-05 18:07:51 +0800

    Add setup for jwt authentication

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Requests\FormRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController implements FormRequest
@@ -26,5 +27,15 @@ class Controller extends BaseController implements FormRequest
     public function getParams(): Request
     {
         return $this->request->replace($this->params);
+    }
+
+    //Add this method to the Controller class
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::factory()->getTTL() * 60
+        ], 200);
     }
 }
